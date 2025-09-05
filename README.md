211dec415bf6e3ef1fac1bd01702f7d6f71f22ff
FadiKrdiyeh <66fadiiiiii66@gmail.com>
2025-09-05 20:14:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fk6-react-datepicker
+A React datetime picker supporting Gregorian and Hijri calendars using moment and moment-hijri
---
+++ src/DateTimePicker.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import moment, { type Moment } from "moment";
+// import 'moment/locale/en-gb'
+import "moment-hijri";
+
+export interface DateTimePickerProps {
+  value?: string;
+  onChange?: (value: string) => void;
+  calendar?: "gregorian" | "hijri";
+  format?: string;
+}
+
+export const DateTimePicker: React.FC<DateTimePickerProps> = (props) => {
+  const initial = props.value
+    ? moment(
+      props.value,
+      props.format ||
+      (props.calendar === "hijri" ? "iYYYY/iMM/iDD" : undefined)
+    )
+    : moment();
+
+  const [date, setDate] = useState<Moment>(initial);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value;
+    const m =
+      props.calendar === "hijri" ? moment(val, "iYYYY/iMM/iDD") : moment(val);
+
+    if (m.isValid()) {
+      setDate(m);
+      props.onChange?.(
+        m.format(
+          props.format ||
+          (props.calendar === "hijri" ? "iYYYY/iMM/iDD" : "YYYY-MM-DD HH:mm")
+        )
+      );
+    }
+  };
+
+  return (
+    <div className="datetime-picker">
+      <input
+        type="text"
+        value={date.format(props.format || (props.calendar === "hijri" ? "iYYYY/iMM/iDD" : "YYYY-MM-DD HH:mm"))}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
